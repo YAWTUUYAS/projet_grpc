30874facca0a8e4ce5b2981d7aeac0d7bf70e7b1
YAWTUUYAS <yassinebenaba17@gmail.com>
2026-04-02 10:49:30 +0200

    ajout de go routines et gestion avec waitgroup/errgroup

--- client/main.go
+++ client/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	checkbookpb "projet_grpc/protofiles/checkbookpb/v1"
 
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 )
 
 func main() {
 	cc, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
+	// var wg sync.WaitGroup
+	eg, _ := errgroup.WithContext(context.Background())
 
 	if err != nil {
 		panic(err)
@@ -19,17 +23,54 @@ func main() {
 	defer cc.Close()
 
 	c := checkbookpb.NewCheckbookServiceClient(cc)
+	/**
+	wg.Add(3)
+	go createCheckbook(checkbookpb.Pages_PAGES_TWENTY_FIVE, "account_001", c)
+	go createCheckbook(checkbookpb.Pages_PAGES_FIFTY, "account_001", c)
+	go createCheckbook(checkbookpb.Pages_PAGES_FIFTY, "account_002", c)
+	wg.Wait()
 
-	createCheckbook(checkbookpb.Pages_PAGES_TWENTY_FIVE, "account_001", c)
-	createCheckbook(checkbookpb.Pages_PAGES_FIFTY, "account_001", c)
-
-	getCheckbooks("account_001", c)
+	wg.Add(2)
+	go getCheckbooks("account_001", c)
+	go getCheckbooks("account_002", c)
+	wg.Done()
 
 	updateCheckbook(9, "account_002", checkbookpb.Pages_PAGES_FIFTY, c)
-	getCheckbooks("account_002", c)
+	**/
+
+	eg, ctx := errgroup.WithContext(context.Background())
+	eg.SetLimit(3)
+	eg.Go(func() error {
+		return createCheckbook(checkbookpb.Pages_PAGES_TWENTY_FIVE, "account_001", c)
+	})
+	eg.Go(func() error {
+		return createCheckbook(checkbookpb.Pages_PAGES_FIFTY, "account_001", c)
+	})
+	eg.Go(func() error {
+		return createCheckbook(checkbookpb.Pages_PAGES_FIFTY, "account_002", c)
+	})
+	if err := eg.Wait(); err != nil {
+		log.Println("Create error:", err)
+		return
+	}
+
+	eg2, ctx := errgroup.WithContext(ctx)
+	eg2.SetLimit(2)
+	eg2.Go(func() error {
+		return getCheckbooks("account_001", c)
+	})
+	eg2.Go(func() error {
+		return getCheckbooks("account_002", c)
+	})
+	if err := eg2.Wait(); err != nil {
+		log.Println("Get error:", err)
+		return
+	}
+	fmt.Printf("all go routines completed")
+
 }
 
-func createCheckbook(nbPage checkbookpb.Pages, accountId string, c checkbookpb.CheckbookServiceClient) {
+func createCheckbook(nbPage checkbookpb.Pages, accountId string, c checkbookpb.CheckbookServiceClient) error {
 	log.Println("creating checkbook")
 
 	res, err := c.CreateCheckbook(context.Background(), &checkbookpb.CreateCheckbookRequest{
@@ -39,13 +80,14 @@ func createCheckbook(nbPage checkbookpb.Pages, accountId string, c checkbookpb.C
 
 	if err != nil {
 		log.Println("error: ", err)
-		panic(err)
+		return err
 	}
 
 	log.Println(res.AccountId, res.CreationDate, res.Id, res.NbPage)
+	return nil
 }
 
-func getCheckbooks(accountId string, c checkbookpb.CheckbookServiceClient) {
+func getCheckbooks(accountId string, c checkbookpb.CheckbookServiceClient) error {
 	log.Println("fetching checkbooks for:", accountId)
 
 	res, err := c.GetCheckbooks(context.Background(), &checkbookpb.GetCheckbooksRequest{
@@ -54,7 +96,7 @@ func getCheckbooks(accountId string, c checkbookpb.CheckbookServiceClient) {
 
 	if err != nil {
 		log.Println("error:", err)
-		return
+		return err
 	}
 
 	for _, cb := range res.Checkbooks {
@@ -62,6 +104,7 @@ func getCheckbooks(accountId string, c checkbookpb.CheckbookServiceClient) {
 			"Pages:", cb.NbPage,
 			"Date:", cb.CreationDate)
 	}
+	return nil
 }
 
 func updateCheckbook(checkbookId int32, accountId string, nbPage checkbookpb.Pages, c checkbookpb.CheckbookServiceClient) {
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.26.1
 
 require (
 	github.com/stretchr/testify v1.11.1
+	golang.org/x/sync v0.19.0
 	google.golang.org/grpc v1.79.3
 	google.golang.org/protobuf v1.36.11
 )
